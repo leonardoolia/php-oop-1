42ea0c1e9f699933ba62abbfb65c0eca39faf03d
leonardoolia <leonardooliaa@gmail.com>
2024-02-23 16:22:25 +0100

    bonus 1

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ class Movie
 // Funzione per avere le informazioni sul film
     public function getMovieInfo()
     {
-        return "Titolo:{$this->title} Regista: {$this->director} Attore principale: {$this->actor} Uscita: {$this->year} Trama: {$this->plot}";
+        return "Titolo:{$this->title} Regista: {$this->toString()} Attore principale: {$this->actor} Uscita: {$this->year} Trama: {$this->plot}";
     }
 
 }
@@ -30,19 +30,24 @@ class Movie
 // ?Dichiarazione classe Director
 class Director
 {
+    public $name;
     public $age;
-    public $filmography;
 
-    public function __construct($age, $filmography)
+    public function __construct($name, $age)
     {
+        $this->name = $name;
         $this->age = $age;
-        $this->filmography = $filmography;
+    }
+
+    public function toString()
+    {
+        return "nome: {$this->name} età: {$this->age}";
     }
 }
 
 // Oggetti che vengono istanziati
-$director1 = new Director(60, ['Avatar', 'Titanic']);
-$director2 = new Director(50, ['Inception', 'Interstellar']);
+$director1 = new Director('James Cameron', 60);
+$director2 = new Director('Christopher Nolan', 50);
 
 $movie1 = new Movie('Avatar', $director1, 'Sam Worthington', 2009, 'Umani vanno su Pandora');
 $movie2 = new Movie('Interstellar', $director2, 'Matthew McConaughey', 2014, 'Un gruppo di astronauti cercano nuovi pianeti abitabili');
