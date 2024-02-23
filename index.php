95857c3d5c9de1796846e310a0c6bedacb468f0a
leonardoolia <leonardooliaa@gmail.com>
2024-02-23 15:58:32 +0100

    bonus 1

--- index.php
+++ index.php
@@ -1,33 +1,52 @@
 <?php
 
-// Dichiarazione classe Movie
-class Movie {
-public $title;
-public $director;
-public $actor;
-public $year;
-public $plot;
+// ?Dichiarazione classe Movie
+class Movie 
+{
+    public $title;
+    public $director;
+    public $actor;
+    public $year;
+    public $plot;
 
 // Aggiunto costruttore
-public function __construct($title, $director, $actor, $year, $plot)
-{
-    $this->title = $title;
-    $this->director = $director;
-    $this->actor = $actor;
-    $this->year = $year;
-    $this->plot = $plot;
-}
+    public function __construct($title, Director $director, $actor, $year, $plot)
+    {
+        $this->title = $title   ;
+        $this->director = $director;
+        $this->actor = $actor;
+        $this->year = $year;
+        $this->plot = $plot;
+    }
 
 // Funzione per avere le informazioni sul film
-public function getMovieInfo()
+    public function getMovieInfo()
+    {
+        return "Titolo:{$this->title} Regista: {$this->director} Attore principale: {$this->actor} Uscita: {$this->year} Trama: {$this->plot}";
+    }
+
+}
+
+// ?Dichiarazione classe Director
+class Director
 {
-    return "Titolo:{$this->title} Regista: {$this->director} Attore principale: {$this->actor} Uscita: {$this->year} Trama: {$this->plot}";
+    public $age;
+    public $filmography;
+
+    public function __construct($age, $filmography)
+    {
+        $this->age = $age;
+        $this->filmography = $filmography;
+    }
 }
 
-}
+// Oggetti che vengono istanziati
+$director1 = new Director(60, ['Avatar', 'Titanic']);
+$director2 = new Director(50, ['Inception', 'Interstellar']);
 
-$movie1 = new Movie('Avatar', 'James Cameron', 'Sam Worthington', 2009, 'Umani vanno su Pandora');
-$movie2 = new Movie('Interstellar', 'Christopher Nolan', 'Matthew McConaughey', 2014, 'Un gruppo di astronauti cercano nuovi pianeti abitabili');
+$movie1 = new Movie('Avatar', $director1, 'Sam Worthington', 2009, 'Umani vanno su Pandora');
+$movie2 = new Movie('Interstellar', $director2, 'Matthew McConaughey', 2014, 'Un gruppo di astronauti cercano nuovi pianeti abitabili');
+
 
 // Stampa delle informazioni in pagina
 echo $movie1->getMovieInfo();
